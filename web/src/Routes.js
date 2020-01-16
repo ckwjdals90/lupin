f45ceda6e7ad6b70a661e3d348205bb61d7c8a10
Jungmin Cha <ckwjdals90@gmail.com>
2020-01-15 20:46:07 -0800

    linking redux to login and signin pages

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import Home from './containers/Home';
-import Login from './containers/Login';
-import Signup from './containers/Signup';
-import NotFound from './components/NotFound';
+import { Home, Login, Signup, } from './containers';
+import { NotFound } from './components';
 
 
 const Routes = () => (
